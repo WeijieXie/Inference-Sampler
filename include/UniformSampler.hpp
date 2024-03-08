a0b3f7d800937e41f9762bc040878022d1b81cca
Weijie Xie <xwj.harry@gmail.com>
2024-03-08 19:48:37 +0000

    derive UniformSampler class from Sampler class

---
+++ include/UniformSampler.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Sampler.hpp"
+
+#include <cmath>
+
+template <typename REAL>
+class UniformSampler:public Sampler<REAL>
+{
+public:
+    UniformSampler(std::string filePath,std::function<REAL(REAL, const std::vector<REAL>&)> modelFunc, std::vector<ParamInfo<REAL>> paraInfo):Sampler<REAL>(filePath,modelFunc, paraInfo){}
+    void sample();
+};
