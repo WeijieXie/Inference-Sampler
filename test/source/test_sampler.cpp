e5e19461edd7dc3fa2c2d986858479292c51ce48
Weijie Xie <xwj.harry@gmail.com>
2024-03-09 19:08:25 +0000

    test sample points generation in 2d and 3d and remove tests for float type

--- test/source/test_sampler.cpp
+++ test/source/test_sampler.cpp
@@ -1,7 +1,11 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_all.hpp>
 #include <stdexcept>
+#include <iostream>
+
+#include "data.h"
 #include "Observations.hpp"
+#include "UniformSampler.hpp"
 using namespace Catch::Matchers;
 template<typename REAL>
 void checkFileContents(Observations<REAL> &obs)
@@ -26,16 +30,96 @@ TEST_CASE("Test file reader (double)", "[File Read]")
     obs.loadData("test/test_data/testing_data_2D.txt");
     checkFileContents(obs);
 }
-TEST_CASE("Test file reader (float)", "[File Read]")
-{
-    Observations<float> obs;
-    obs.loadData("test/test_data/testing_data_2D.txt");
-    checkFileContents(obs);
-}
+// TEST_CASE("Test file reader (double)", "[File Read]")
+// {
+//     Observations<double> obs;
+//     obs.loadData("test/test_data/testing_data_2D.txt");
+//     checkFileContents(obs);
+// }
 TEST_CASE("Test file reader (not a number)", "[File Read]")
 {
     Observations<double> obs_double;
     REQUIRE_THROWS_AS(obs_double.loadData("test/test_data/loading_data_exception.txt"),std::invalid_argument);
-    Observations<float> obs_float;
-    REQUIRE_THROWS_AS(obs_float.loadData("test/test_data/loading_data_exception.txt"),std::invalid_argument);
+    // Observations<double> obs_double;
+    // REQUIRE_THROWS_AS(obs_double.loadData("test/test_data/loading_data_exception.txt"),std::invalid_argument);
 }
+// TEST_CASE("Test constructor", "[Data Process]")
+// {
+//     Observations<double> obs_double;
+//     REQUIRE_THROWS_AS(obs_double.loadData("test/test_data/loading_data_exception.txt"),std::invalid_argument);
+//     // Observations<double> obs_double;
+//     // REQUIRE_THROWS_AS(obs_double.loadData("test/test_data/loading_data_exception.txt"),std::invalid_argument);
+// }
+// TEST_CASE("Test setters and getters", "[Data Process]")
+// {
+//     Observations<double> obs_double;
+//     REQUIRE_THROWS_AS(obs_double.loadData("test/test_data/loading_data_exception.txt"),std::invalid_argument);
+//     // Observations<double> obs_double;
+//     // REQUIRE_THROWS_AS(obs_double.loadData("test/test_data/loading_data_exception.txt"),std::invalid_argument);
+// }
+TEST_CASE("Test sample points 2D", "[Data Process]")
+{
+    // std::vector<double> params = {1.0, 2.0, 3.0};
+    // double x = 5.0;
+
+    // std::function<double(double, const std::vector<double>&)> modelFunc;
+    // modelFunc = [](double x, const std::vector<double>& params) -> double {
+    //     double result =  params[0]*x + params[1]; // Example implementation
+    //     return result;
+    // };
+
+    // std::vector<ParamInfo<double>> paraVec;
+    // ParamInfo<double> paraInfo1(0,1,"para1");
+    // ParamInfo<double> paraInfo2(0,1,"para2");
+    // paraVec.push_back(paraInfo1);
+    // paraVec.push_back(paraInfo2);
+
+    std::string filePath("test/test_data/testing_data_2D.txt");
+    UniformSampler<double> uniformSampler(filePath,test_data::modelFunc,test_data::paraVec_2d);
+    uniformSampler.numBinsSetter(3);
+    uniformSampler.sample();
+    std::vector<std::vector<double>> mat = uniformSampler.sampledChainGetter();
+    REQUIRE(mat.size() == test_data::sample_points_2d.size());
+    REQUIRE(mat[0].size() == test_data::sample_points_2d[0].size()+1);
+    // std::cout<<uniformSampler.sampledChainGetter().size()<<std::endl;
+    // std::vector<std::vector<double>> mat_t = {{1,2,3},{4,5,6},{7,8,9}};
+    double val = 0;
+    for (int i = 0; i < int(mat.size()); i++) {
+        for (int j = 0; j < int(test_data::sample_points_2d[0].size()); j++) {
+            val = test_data::sample_points_2d[i][j];
+            REQUIRE_THAT(mat[i][j], WithinRel(val, 0.0001));
+        }
+    }
+    // for (const auto &row : mat) {
+    //     for (double val : row) {
+    //         // REQUIRE_THAT(obs.outputs[0], WithinRel(0.97454, 0.0001));
+
+    //         std::cout << val << " ";
+    //     }
+    //     std::cout << '\n';
+    // }
+    // std::cout<<uniformSampler.sampledChainGetter()<<std::endl;
+}
+
+TEST_CASE("Test sample points 3D", "[Data Process]")
+{
+    std::string filePath("test/test_data/testing_data_2D.txt");
+    UniformSampler<double> uniformSampler(filePath,test_data::modelFunc,test_data::paraVec_3d);
+    uniformSampler.numBinsSetter(3);
+    uniformSampler.sample();
+    std::vector<std::vector<double>> mat = uniformSampler.sampledChainGetter();
+    REQUIRE(mat.size() == test_data::sample_points_3d.size());
+    REQUIRE(mat[0].size() == test_data::sample_points_3d[0].size()+1);
+    // std::cout<<uniformSampler.sampledChainGetter().size()<<std::endl;
+    // std::vector<std::vector<double>> mat_t = {{1,2,3},{4,5,6},{7,8,9}};
+    double val = 0;
+    for (int i = 0; i < int(mat.size()); i++) {
+        for (int j = 0; j < int(test_data::sample_points_3d[0].size()); j++) {
+            val = test_data::sample_points_3d[i][j];
+            REQUIRE_THAT(mat[i][j], WithinRel(val, 0.0001));
+        }
+    }
+    // std::cout<<uniformSampler.sampledChainGetter()<<std::endl;
+}
+
+
