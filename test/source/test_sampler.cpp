e371213e8888beafa15ca46821a44b75a1196c11
Weijie Xie <xwj.harry@gmail.com>
2024-03-09 19:53:04 +0000

    test the liklihood function in 2d

--- test/source/test_sampler.cpp
+++ test/source/test_sampler.cpp
@@ -122,4 +122,55 @@ TEST_CASE("Test sample points 3D", "[Data Process]")
     // std::cout<<uniformSampler.sampledChainGetter()<<std::endl;
 }
 
+TEST_CASE("Test likelihood 2D", "[Data Process]")
+{
+    // std::vector<double> params = {1.0, 2.0, 3.0};
+    // double x = 5.0;
 
+    // std::function<double(double, const std::vector<double>&)> modelFunc;
+    // modelFunc = [](double x, const std::vector<double>& params) -> double {
+    //     double result =  params[0]*x + params[1]; // Example implementation
+    //     return result;
+    // };
+
+    // std::vector<ParamInfo<double>> paraVec;
+    // ParamInfo<double> paraInfo1(0,1,"para1");
+    // ParamInfo<double> paraInfo2(0,1,"para2");
+    // paraVec.push_back(paraInfo1);
+    // paraVec.push_back(paraInfo2);
+
+    std::string filePath("test/test_data/testing_data_2D.txt");
+    UniformSampler<double> uniformSampler(filePath,test_data::modelFunc,test_data::paraVec_2d);
+    uniformSampler.numBinsSetter(3);
+    uniformSampler.sample();
+    std::vector<std::vector<double>> mat = uniformSampler.sampledChainGetter();
+    REQUIRE(mat.size() == test_data::sample_points_2d.size());
+    REQUIRE(mat[0].size() == test_data::sample_points_2d[0].size()+1);
+    // std::cout<<uniformSampler.sampledChainGetter().size()<<std::endl;
+    // std::vector<std::vector<double>> mat_t = {{1,2,3},{4,5,6},{7,8,9}};
+    
+    double likeliHood = 0;
+    double val = 0;
+ 
+    // std::cout<<std::endl;
+    for (int i = 0; i < int(mat.size()); i++) {
+        likeliHood = std::exp(mat[i].back());
+        val = test_data::sample_likelihoods[i];
+        REQUIRE_THAT(likeliHood, WithinRel(val, 0.01));
+        // std::cout << std::exp(mat[i].back()) << " ";
+    }
+    // std::cout<<std::endl;
+    // for (double val : test_data::sample_likelihoods) {
+    //     // REQUIRE_THAT(obs.outputs[0], WithinRel(0.97454, 0.0001));
+    //     std::cout << val << " ";
+    // }
+    // for (const auto &row : mat) {
+    //     for (double val : row) {
+    //         // REQUIRE_THAT(obs.outputs[0], WithinRel(0.97454, 0.0001));
+
+    //         std::cout << val << " ";
+    //     }
+    //     std::cout << '\n';
+    // }
+    // std::cout<<uniformSampler.sampledChainGetter()<<std::endl;
+}
