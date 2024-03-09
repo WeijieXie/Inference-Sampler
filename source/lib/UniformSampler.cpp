43168faa457c0ff051cae37e06b444f91001b4f7
Weijie Xie <xwj.harry@gmail.com>
2024-03-09 19:52:24 +0000

    comment all the printings

--- source/lib/UniformSampler.cpp
+++ source/lib/UniformSampler.cpp
@@ -21,10 +21,10 @@ void UniformSampler<REAL>::sample()
     for(auto iter = this->paraInfo.begin(); iter < this->paraInfo.end(); iter++)
     {
         mins.push_back(iter->min);
-        std::cout<<"min: "<<iter->min<<std::endl;
+        // std::cout<<"min: "<<iter->min<<std::endl;
         width = (iter->max-iter->min)/this->numBins;
         widths.push_back(width);
-        std::cout<<"width: "<<width<<std::endl;
+        // std::cout<<"width: "<<width<<std::endl;
     }
 
     for(int i = 0; i < numSample; i++)
@@ -35,18 +35,18 @@ void UniformSampler<REAL>::sample()
         {
             // std::cout<<"size1: "<<sample.size()<<std::endl;
             index = (i%int(pow(this->numBins,j+1)))/(int(pow(this->numBins,j)));
-            std::cout<<"index"<<j<<": "<<index<<std::endl;
+            // std::cout<<"index"<<j<<": "<<index<<std::endl;
             paraValue = mins.at(j) + (index+0.5)*widths.at(j);
             std::cout<<"paraValue"<<j<<","<<index<<": "<<paraValue<<std::endl;
             sample.push_back(paraValue);
         }
         // std::cout<<"size2: "<<sample.size()<<std::endl;
-        std::cout<<"this->likelihood(sample): "<<this->likelihood(sample)<<std::endl;
+        // std::cout<<"this->likelihood(sample): "<<this->likelihood(sample)<<std::endl;
         sample.push_back(this->likelihood(sample));
         // std::cout<<"size3: "<<sample.size()<<std::endl;
-        for (REAL val : sample) {
-            std::cout << val << std::endl;
-        }
+        // for (REAL val : sample) {
+        //     std::cout << val << std::endl;
+        // }
         this->sampledChain.push_back(sample);
     }
 }
