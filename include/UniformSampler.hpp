d6cdc09f25c3292fdeb351007afb9ddb097927fc
Weijie Xie <xwj.harry@gmail.com>
2024-03-09 19:02:55 +0000

    derive default constructor from Sampler class

--- include/UniformSampler.hpp
+++ include/UniformSampler.hpp
@@ -9,5 +9,6 @@ class UniformSampler:public Sampler<REAL>
 {
 public:
     UniformSampler(std::string filePath,std::function<REAL(REAL, const std::vector<REAL>&)> modelFunc, std::vector<ParamInfo<REAL>> paraInfo):Sampler<REAL>(filePath,modelFunc, paraInfo){}
+    UniformSampler():Sampler<REAL>(){}
     void sample();
 };
