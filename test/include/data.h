afbd5f44ccbd27b56e26136d41d0cababc6791ac
Weijie Xie <xwj.harry@gmail.com>
2024-03-09 19:04:16 +0000

    add data for test

--- test/include/data.h
+++ test/include/data.h
@@ -1,5 +1,6 @@
 #pragma once 
 
+#include "Sampler.hpp"
 #include <vector>
 
 class test_data
@@ -8,4 +9,19 @@ public:
     static const std::vector<double> sample_likelihoods;
 
     static const std::vector<std::vector<double>> sample_points_2d;
+
+    static const std::vector<std::vector<double>> sample_points_3d;
+
+    static const std::function<double(double, const std::vector<double>&)> modelFunc;
+
+    static const ParamInfo<double> paraInfo1;
+    static const ParamInfo<double> paraInfo2;
+    static const ParamInfo<double> paraInfo3;
+
+    static const std::vector<ParamInfo<double>> paraVec_2d;
+
+    static const std::vector<ParamInfo<double>> paraVec_3d;
+
+    // static UniformSampler<double> uniformSampler;
+
 };
--- test/source/data.cpp
+++ test/source/data.cpp
@@ -5,3 +5,25 @@ const std::vector<double> test_data::sample_likelihoods = {0.575187, 0.846482, 0
 const std::vector<std::vector<double>> test_data::sample_points_2d = {{1.0/6, 1.0/6}, {0.5, 1.0/6}, {5.0/6, 1.0/6},
                                                                       {1.0/6, 0.5}, {0.5, 0.5}, {5.0/6, 0.5},
                                                                       {1.0/6, 5.0/6}, {0.5, 5.0/6}, {5.0/6, 5.0/6}};
+
+const std::vector<std::vector<double>> test_data::sample_points_3d = {{1.0/6, 1.0/6,1.0/6}, {0.5, 1.0/6,1.0/6}, {5.0/6, 1.0/6,1.0/6},
+                                                                      {1.0/6, 0.5,1.0/6}, {0.5, 0.5,1.0/6}, {5.0/6, 0.5,1.0/6},
+                                                                      {1.0/6, 5.0/6,1.0/6}, {0.5, 5.0/6,1.0/6}, {5.0/6, 5.0/6,1.0/6},
+                                                                      {1.0/6, 1.0/6, 0.5}, {0.5, 1.0/6, 0.5}, {5.0/6, 1.0/6, 0.5},
+                                                                      {1.0/6, 0.5, 0.5}, {0.5, 0.5, 0.5}, {5.0/6, 0.5, 0.5},
+                                                                      {1.0/6, 5.0/6, 0.5}, {0.5, 5.0/6, 0.5}, {5.0/6, 5.0/6, 0.5},
+                                                                      {1.0/6, 1.0/6, 5.0/6}, {0.5, 1.0/6, 5.0/6}, {5.0/6, 1.0/6, 5.0/6},
+                                                                      {1.0/6, 0.5, 5.0/6}, {0.5, 0.5, 5.0/6}, {5.0/6, 0.5, 5.0/6},
+                                                                      {1.0/6, 5.0/6, 5.0/6}, {0.5, 5.0/6, 5.0/6}, {5.0/6, 5.0/6, 5.0/6}};
+
+const std::function<double(double, const std::vector<double>&)> test_data::modelFunc = [](double x, const std::vector<double>& params) -> double {
+                                                                                        double result =  params[0]*x + params[1]; // Example implementation
+                                                                                        return result;
+                                                                                    };
+
+const ParamInfo<double> test_data::paraInfo1=ParamInfo<double>(0,1,"para1");
+const ParamInfo<double> test_data::paraInfo2=ParamInfo<double>(0,1,"para2");
+const ParamInfo<double> test_data::paraInfo3=ParamInfo<double>(0,1,"para3");
+
+const std::vector<ParamInfo<double>> test_data::paraVec_2d = {paraInfo1,paraInfo2};
+const std::vector<ParamInfo<double>> test_data::paraVec_3d = {paraInfo1,paraInfo2,paraInfo3};
