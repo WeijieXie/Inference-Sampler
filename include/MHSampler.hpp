36bdfc25e5fc35235e35cff12579e0c90c8d05f8
Weijie Xie <xwj.harry@gmail.com>
2024-03-13 00:46:34 +0000

    create new class MHSampler and implement the constructor

---
+++ include/MHSampler.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Sampler.hpp"
+
+template <typename REAL>
+class MHSampler : public Sampler<REAL>
+{
+    prtected : int numPoints = 100000;
+    REAL stepSize = 0.01;
+
+public:
+    MHSampler(std::string filePath, std::function<REAL(REAL, const std::vector<REAL> &)> modelFunc, std::vector<ParamInfo<REAL>> paraInfo, int numBins, int numPoints, REAL stepSize);
+    MHSampler() : Sampler<REAL>(){};
+    void sample();
+};
---
+++ source/lib/MHSampler.cpp
@@ -0,0 +1,11 @@
+#include "MHSampler.hpp"
+
+template <typename REAL>
+MHSampler<REAL>::MHSampler(std::string filePath, std::function<REAL(REAL, const std::vector<REAL> &)> modelFunc, std::vector<ParamInfo<REAL>> paraInfo, int numBins, int numPoints, REAL stepSize) : Sampler<REAL>(filePath, modelFunc, paraInfo)
+{
+    this->numPoints = numPoints;
+    this->stepSize = stepSize;
+}
+
+template MHSampler<float>::MHSampler(std::string filePath, std::function<float(float, const std::vector<float> &)> modelFunc, std::vector<ParamInfo<float>> paraInfo, int numBins, int numPoints, float stepSize);
+template MHSampler<double>::MHSampler(std::string filePath, std::function<double(double, const std::vector<double> &)> modelFunc, std::vector<ParamInfo<double>> paraInfo, int numBins, int numPoints, double stepSize);
