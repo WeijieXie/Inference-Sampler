135c6ee462bfdab6b412bd5d2a6b98053e9675ca
Weijie Xie <xwj.harry@gmail.com>
2024-03-14 13:03:48 +0000

    test the constructor of uniform sampling

--- test/source/test_sampler.cpp
+++ test/source/test_sampler.cpp
@@ -31,50 +31,32 @@ TEST_CASE("Test file reader (double)", "[File Read]")
     obs.loadData("test/test_data/testing_data_2D.txt");
     checkFileContents(obs);
 }
-// TEST_CASE("Test file reader (double)", "[File Read]")
-// {
-//     Observations<double> obs;
-//     obs.loadData("test/test_data/testing_data_2D.txt");
-//     checkFileContents(obs);
-// }
-TEST_CASE("Test file reader (not a number)", "[File Read]")
+TEST_CASE("Test file reader (exceptions)", "[File Read]")
 {
     Observations<double> obs_double;
     REQUIRE_THROWS_AS(obs_double.loadData("test/test_data/loading_data_exception.txt"), std::invalid_argument);
-    // Observations<double> obs_double;
-    // REQUIRE_THROWS_AS(obs_double.loadData("test/test_data/loading_data_exception.txt"),std::invalid_argument);
 }
-// TEST_CASE("Test constructor", "[Data Process]")
-// {
-//     Observations<double> obs_double;
-//     REQUIRE_THROWS_AS(obs_double.loadData("test/test_data/loading_data_exception.txt"),std::invalid_argument);
-//     // Observations<double> obs_double;
-//     // REQUIRE_THROWS_AS(obs_double.loadData("test/test_data/loading_data_exception.txt"),std::invalid_argument);
-// }
-// TEST_CASE("Test setters and getters", "[Data Process]")
-// {
-//     Observations<double> obs_double;
-//     REQUIRE_THROWS_AS(obs_double.loadData("test/test_data/loading_data_exception.txt"),std::invalid_argument);
-//     // Observations<double> obs_double;
-//     // REQUIRE_THROWS_AS(obs_double.loadData("test/test_data/loading_data_exception.txt"),std::invalid_argument);
-// }
+TEST_CASE("Test UniformSampler constructor", "[Constructor]")
+{
+    std::string filePath("test/test_data/testing_data_2D.txt");
+    UniformSampler<double> uniformSamplerA(filePath, test_data::modelFunc, test_data::paraVec_2d);
+    REQUIRE(uniformSamplerA.marDisGetter().size() == 2);
+    REQUIRE(uniformSamplerA.marDisGetter()[0].size() == 100);
+
+    UniformSampler<double> uniformSamplerB(filePath, test_data::modelFunc, test_data::paraVec_3d, 13);
+    REQUIRE(uniformSamplerB.marDisGetter().size() == 3);
+    REQUIRE(uniformSamplerB.marDisGetter()[0].size() == 13);
+}
+TEST_CASE("Test UniformSampler constructor (exceptions)", "[Constructor]")
+{
+    std::string filePath("test/test_data/testing_data_2D.txt");
+    REQUIRE_THROWS_AS(UniformSampler<double>(filePath, test_data::modelFunc, test_data::paraVec_2d, 0), std::domain_error);
+
+    std::string filePathE("test/test_data/loading_data_exception.txt");
+    REQUIRE_THROWS_AS(UniformSampler<double>(filePathE, test_data::modelFunc, test_data::paraVec_2d, 10), std::invalid_argument);
+}
 TEST_CASE("Test sample points 2D", "[Data Process]")
 {
-    // std::vector<double> params = {1.0, 2.0, 3.0};
-    // double x = 5.0;
-
-    // std::function<double(double, const std::vector<double>&)> modelFunc;
-    // modelFunc = [](double x, const std::vector<double>& params) -> double {
-    //     double result =  params[0]*x + params[1]; // Example implementation
-    //     return result;
-    // };
-
-    // std::vector<ParamInfo<double>> paraVec;
-    // ParamInfo<double> paraInfo1(0,1,"para1");
-    // ParamInfo<double> paraInfo2(0,1,"para2");
-    // paraVec.push_back(paraInfo1);
-    // paraVec.push_back(paraInfo2);
-
     std::string filePath("test/test_data/testing_data_2D.txt");
     UniformSampler<double> uniformSampler(filePath, test_data::modelFunc, test_data::paraVec_2d);
     uniformSampler.numBinsSetter(3);
@@ -93,15 +75,6 @@ TEST_CASE("Test sample points 2D", "[Data Process]")
             REQUIRE_THAT(mat[i][j], WithinRel(val, 0.0001));
         }
     }
-    // for (const auto &row : mat) {
-    //     for (double val : row) {
-    //         // REQUIRE_THAT(obs.outputs[0], WithinRel(0.97454, 0.0001));
-
-    //         std::cout << val << " ";
-    //     }
-    //     std::cout << '\n';
-    // }
-    // std::cout<<uniformSampler.sampledChainGetter()<<std::endl;
 }
 
 TEST_CASE("Test sample points 3D", "[Data Process]")
@@ -113,8 +86,6 @@ TEST_CASE("Test sample points 3D", "[Data Process]")
     std::vector<std::vector<double>> mat = uniformSampler.sampledChainGetter();
     REQUIRE(mat.size() == test_data::sample_points_3d.size());
     REQUIRE(mat[0].size() == test_data::sample_points_3d[0].size() + 1);
-    // std::cout<<uniformSampler.sampledChainGetter().size()<<std::endl;
-    // std::vector<std::vector<double>> mat_t = {{1,2,3},{4,5,6},{7,8,9}};
     double val = 0;
     for (int i = 0; i < int(mat.size()); i++)
     {
@@ -124,59 +95,25 @@ TEST_CASE("Test sample points 3D", "[Data Process]")
             REQUIRE_THAT(mat[i][j], WithinRel(val, 0.0001));
         }
     }
-    // std::cout<<uniformSampler.sampledChainGetter()<<std::endl;
 }
 
 TEST_CASE("Test likelihood 2D", "[Data Process]")
 {
-    // std::vector<double> params = {1.0, 2.0, 3.0};
-    // double x = 5.0;
-
-    // std::function<double(double, const std::vector<double>&)> modelFunc;
-    // modelFunc = [](double x, const std::vector<double>& params) -> double {
-    //     double result =  params[0]*x + params[1]; // Example implementation
-    //     return result;
-    // };
-
-    // std::vector<ParamInfo<double>> paraVec;
-    // ParamInfo<double> paraInfo1(0,1,"para1");
-    // ParamInfo<double> paraInfo2(0,1,"para2");
-    // paraVec.push_back(paraInfo1);
-    // paraVec.push_back(paraInfo2);
-
     std::string filePath("test/test_data/testing_data_2D.txt");
     UniformSampler<double> uniformSampler(filePath, test_data::modelFunc, test_data::paraVec_2d);
     uniformSampler.numBinsSetter(3);
     uniformSampler.sample();
     std::vector<std::vector<double>> mat = uniformSampler.sampledChainGetter();
-    // std::cout<<uniformSampler.sampledChainGetter().size()<<std::endl;
-    // std::vector<std::vector<double>> mat_t = {{1,2,3},{4,5,6},{7,8,9}};
 
     double likeliHood = 0;
     double val = 0;
 
-    // std::cout<<std::endl;
     for (int i = 0; i < int(mat.size()); i++)
     {
         likeliHood = std::exp(mat[i].back());
         val = test_data::sample_likelihoods[i];
         REQUIRE_THAT(likeliHood, WithinRel(val, 0.01));
-        // std::cout << std::exp(mat[i].back()) << " ";
     }
-    // std::cout<<std::endl;
-    // for (double val : test_data::sample_likelihoods) {
-    //     // REQUIRE_THAT(obs.outputs[0], WithinRel(0.97454, 0.0001));
-    //     std::cout << val << " ";
-    // }
-    // for (const auto &row : mat) {
-    //     for (double val : row) {
-    //         // REQUIRE_THAT(obs.outputs[0], WithinRel(0.97454, 0.0001));
-
-    //         std::cout << val << " ";
-    //     }
-    //     std::cout << '\n';
-    // }
-    // std::cout<<uniformSampler.sampledChainGetter()<<std::endl;
 }
 TEST_CASE("Test Marginal Distribution 2D", "[Data Process]")
 {
@@ -185,13 +122,11 @@ TEST_CASE("Test Marginal Distribution 2D", "[Data Process]")
     uniformSampler.numBinsSetter(3);
     uniformSampler.sample();
     std::vector<std::vector<double>> mat = uniformSampler.marDisGetter();
-    // std::cout << mat.size() << std::endl;
     REQUIRE(mat.size() == test_data::marginal_distribution_2d.size());
     REQUIRE(mat[0].size() == test_data::marginal_distribution_2d[0].size());
 
     double val = 0;
 
-    // std::cout<<std::endl;
     for (int i = 0; i < int(mat.size()); i++)
     {
         for (int j = 0; j < int(mat[0].size()); j++)
@@ -200,20 +135,6 @@ TEST_CASE("Test Marginal Distribution 2D", "[Data Process]")
             REQUIRE_THAT(mat[i][j], WithinRel(val, 0.0001));
         }
     }
-    // std::cout<<std::endl;
-    // for (double val : test_data::sample_likelihoods) {
-    //     // REQUIRE_THAT(obs.outputs[0], WithinRel(0.97454, 0.0001));
-    //     std::cout << val << " ";
-    // }
-    // for (const auto &row : mat) {
-    //     for (double val : row) {
-    //         // REQUIRE_THAT(obs.outputs[0], WithinRel(0.97454, 0.0001));
-
-    //         std::cout << val << " ";
-    //     }
-    //     std::cout << '\n';
-    // }
-    // std::cout<<uniformSampler.sampledChainGetter()<<std::endl;
 }
 
 TEST_CASE("Test Summary Calculator", "[Data Process]")
@@ -240,20 +161,12 @@ TEST_CASE("Test Summary Calculator", "[Data Process]")
     REQUIRE(paraStdDevs[0] == paraStdDevs[1]);
 }
 
-TEST_CASE("Test", "[Data Process]")
+TEST_CASE("Test MHSampler (2 paras)", "[Data Process]")
 {
-    // std::random_device rd;
-    // std::default_random_engine eng(rd());
-    // std::normal_distribution<double> normalDist(0.0, 1.0);
-    // std::uniform_real_distribution<double> uniformDist(0.0, 1.0);
-    // for (int j = 0; j < 10; j++)
-    // {
-    //    std::cout<<normalDist(eng)<<"+"<<uniformDist(eng)<<std::endl;
-    // }
     std::string filePath("test/test_data/testing_data_2D.txt");
     std::function<double(double, const std::vector<double> &)> modelFunc = [](double x, const std::vector<double> &params) -> double
     {
-        double result = params[0] * (1/x)- params[1] * x; // Example implementation
+        double result = params[0] * (1 / x) - params[1] * x; // Example implementation
         // double result = params[0] * x*x*x + params[1] * x*x+ params[2] * x+ params[3];
         return result;
     };
@@ -262,56 +175,53 @@ TEST_CASE("Test", "[Data Process]")
     const ParamInfo<double> paraInfo2 = ParamInfo<double>(0, 5, "b");
     const std::vector<ParamInfo<double>> paraVec_2d = {paraInfo1, paraInfo2};
 
-    // const ParamInfo<double> paraInfo1=ParamInfo<double>(-3,3,"a");
-    // const ParamInfo<double> paraInfo2=ParamInfo<double>(-3,3,"b");
-    // const ParamInfo<double> paraInfo3=ParamInfo<double>(-3,3,"c");
-    // const ParamInfo<double> paraInfo4=ParamInfo<double>(-3,3,"d");
-    // const std::vector<ParamInfo<double>> paraVec_4d = {paraInfo1,paraInfo2,paraInfo3,paraInfo4};
-
     UniformSampler<double> uniformSampler(filePath, modelFunc, paraVec_2d);
-    uniformSampler.numBinsSetter(100);
+    uniformSampler.numBinsSetter(3);
     uniformSampler.sample();
     uniformSampler.summaryCalculator();
 
-    // std::string filePath("test/test_data/testing_data_2D.txt");
-    // std::function<double(double, const std::vector<double> &)> modelFunc = [](double x, const std::vector<double> &params) -> double
-    // {
-    //     double result = params[0] * params[1] * x; // Example implementation
-    //     return result;
-    // };
-
-    int numPoints = 100000;
-    double stepSize = 0.05;
-
-    MHSampler<double> mhSampler(filePath, modelFunc, paraVec_2d, numPoints, stepSize);
-    mhSampler.numBinsSetter(100);
+    MHSampler<double> mhSampler(filePath, modelFunc, paraVec_2d);
+    mhSampler.numBinsSetter(3);
     mhSampler.sample();
     mhSampler.summaryCalculator();
-    mhSampler.marDisPlotter();
 
-    std::vector<std::vector<double>> mat1 = uniformSampler.marDisGetter();
-    // std::cout << mat.size() << " ";
-    for (const auto &row : mat1)
+    REQUIRE(mhSampler.peaksGetter().size() == uniformSampler.peaksGetter().size());
+
+    for (int i = 0; i < int(mhSampler.peaksGetter().size()); i++)
     {
-        for (double val : row)
-        {
-            // REQUIRE_THAT(obs.outputs[0], WithinRel(0.97454, 0.0001));
-
-            std::cout << val << " ";
-        }
-        std::cout << '\n';
-    }
-
-    std::vector<std::vector<double>> mat2 = mhSampler.marDisGetter();
-    // std::cout << mat.size() << " ";
-    for (const auto &row : mat2)
-    {
-        for (double val : row)
-        {
-            // REQUIRE_THAT(obs.outputs[0], WithinRel(0.97454, 0.0001));
-
-            std::cout << val << " ";
-        }
-        std::cout << '\n';
+        REQUIRE(mhSampler.peaksGetter()[i] == uniformSampler.peaksGetter()[i]);
     }
 }
+
+// TEST_CASE("Test MHSampler (4 paras)", "[Data Process]")
+// {
+//     std::string filePath("test/test_data/testing_data_2D.txt");
+//     std::function<double(double, const std::vector<double> &)> modelFunc = [](double x, const std::vector<double> &params) -> double
+//     {
+//         double result = params[0] * x * x * x + params[1] * x * x + params[2] * x + params[3];
+//         return result;
+//     };
+
+//     const ParamInfo<double> paraInfo1 = ParamInfo<double>(-3, 3, "a");
+//     const ParamInfo<double> paraInfo2 = ParamInfo<double>(-3, 3, "b");
+//     const ParamInfo<double> paraInfo3 = ParamInfo<double>(-3, 3, "c");
+//     const ParamInfo<double> paraInfo4 = ParamInfo<double>(-3, 3, "d");
+//     const std::vector<ParamInfo<double>> paraVec_4d = {paraInfo1, paraInfo2, paraInfo3, paraInfo4};
+
+//     UniformSampler<double> uniformSampler(filePath, modelFunc, paraVec_4d);
+//     uniformSampler.numBinsSetter(3);
+//     uniformSampler.sample();
+//     uniformSampler.summaryCalculator();
+
+//     MHSampler<double> mhSampler(filePath, modelFunc, paraVec_4d);
+//     mhSampler.numBinsSetter(3);
+//     mhSampler.sample();
+//     mhSampler.summaryCalculator();
+
+//     REQUIRE(mhSampler.peaksGetter().size() == uniformSampler.peaksGetter().size());
+
+//     for (int i = 0; i < int(mhSampler.peaksGetter().size()); i++)
+//     {
+//         REQUIRE(mhSampler.peaksGetter()[i] == uniformSampler.peaksGetter()[i]);
+//     }
+// }
