4a022eeb09562e02f84197ab2dd56fb0940897cd
Weijie Xie <xwj.harry@gmail.com>
2024-03-14 01:32:20 +0000

    enable UniformSampler to take numBins in its constructor

--- include/UniformSampler.hpp
+++ include/UniformSampler.hpp
@@ -6,7 +6,7 @@ template <typename REAL>
 class UniformSampler:public Sampler<REAL>
 {
 public:
-    UniformSampler(std::string filePath,std::function<REAL(REAL, const std::vector<REAL>&)> modelFunc, std::vector<ParamInfo<REAL>> paraInfo):Sampler<REAL>(filePath,modelFunc, paraInfo){}
+    UniformSampler(std::string filePath,std::function<REAL(REAL, const std::vector<REAL>&)> modelFunc, std::vector<ParamInfo<REAL>> paraInfo,int numBins=100):Sampler<REAL>(filePath,modelFunc, paraInfo,numBins){}
     UniformSampler():Sampler<REAL>(){}
     void sample();
 };
